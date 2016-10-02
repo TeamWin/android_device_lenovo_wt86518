741435908eba78a2e03cbbcd423ef89933487847
IsaacChen <tingyi364@gmail.com>
2016-10-02 09:13:34 -0700

    Build Successfully

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -2,19 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 ## Build and run dtbtool
 DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
-INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
+INSTALLED_DTIMAGE_TARGET := $(LOCAL_PATH)/recovery/dt.img
 
 ifneq ($(TARGET_KERNEL_ARCH),)
 KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
 else
 KERNEL_ARCH := $(TARGET_ARCH)
 endif
-
-$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
-	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
-	$(hide) $(DTBTOOL) -2 -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/
-	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
-
 
 ## Overload bootimg generation: Same as the original, + --dt arg
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
