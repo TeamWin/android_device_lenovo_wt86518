c61f52ce47352faae9e331013b3a3f4c1ce13405
bugerxXx <muhammadiqbal20122012@gmail.com>
2017-01-26 17:18:14 +0700

    wt86518 : Fix Typo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,7 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
 
 # Kernel
 TARGET_USES_UNCOMPRESSED_KERNEL := true
-BBOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
